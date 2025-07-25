5eec614bc61509a7c294f5623a12f1332de3b0d5
jkpark <jkpark@seoitv.com>
2025-07-25 17:23:45 +0900

    tmp2

--- er_calc.py
+++ er_calc.py
@@ -2,6 +2,7 @@
 import yfinance as yf
 import sys
 import pandas as pd
+import queue
 
 
 class ER_calc:
@@ -12,32 +13,73 @@ class ER_calc:
         self.name = name
 
         data = yf.Ticker(ticker)
-        self.df = data.history(period="6y")
+        self.df = data.history(period="11y")
 
         #print(self.df)
 
 
     def calc(self):
-        #period_arr=[1,2,3,4,5,6,7,8,9,10]
-        period_arr=[1,2,3,4,5]
+        period_arr=[1,2,3,4,5,6,7,8,9,10]
+        #period_arr=[1,2,3,4,5]
         total_avr = []
         year = 252
+        stock = 1
+        div_queue = queue.Queue()
         for p in period_arr:
             idx = 0
             avr_list = []
             for close in self.df['Close']:
-                idx = idx+1
+
+                div = self.df['Dividends'][idx]
+                if div != 0.0:
+                    #div_arr.append(div)
+                    #div_st = div/close
+                    div_queue.put(div/close)
+                    if idx > year and div_queue.empty() == False:
+                        div_queue.get()
+                        
+
+                """
+                if idx%((year) * p) == 0 and idx != 0:
+                    #print('clear')
+                    #print(p,idx, list(div_queue.queue)) 
+                    stock = 1
+                    #if div_queue.empty() == False:
+                    #    div_queue.get()
+                """
+
+
                 if(idx < (year+1)*p):
+                    idx = idx+1
                     continue
 
+                #print(p,idx, list(div_queue.queue)) 
                 pre_close = self.df['Close'][idx-(year*p)]
                 #print(close, pre_close, close/pre_close-1)
 
+                #if self.df['Dividends'][idx-(year*p)] != 0.0:
+                #    div_arr.append(self.df['Dividends'][idx-(year*p)])
+
+
+                div_list = list(div_queue.queue)
+                for d in div_list:
+                    stock = stock + d
+
+                #stock = stock + (self.df['Dividends'][idx]/close)
+               
+                
+                """
                 dist = 0
                 for i in range(p):
                     dist = dist + self.df['Close'][idx-(year*(i+1))]*self.dist
+                """
 
-                avr_list.append((close + dist)/pre_close-1)
+                #avr_list.append((close + dist)/pre_close-1)
+                avr_list.append((close*stock)/pre_close - 1)
+
+                stock = 1
+
+                idx = idx+1
 
             try:
                 average = sum(avr_list) / len(avr_list)
@@ -47,8 +89,18 @@ class ER_calc:
                 #print(p,0)
                 total_avr.append(0)
 
+            #print(div_queue)
+            #print(list(div_queue.queue)) 
+
         t_avr = sum(total_avr) / len(total_avr)
-        print(self.name, self.ticker ,round(t_avr*100,2))
+
+        """
+        j = 0
+        for d in total_avr:
+            print(j+1, d)
+            j = j+1
+        """
+        print(self.name, self.ticker ,round(t_avr*100,2), round(min(total_avr)*100,2), round(max(total_avr)*100,2))
 
 
 if __name__ == '__main__':
---
+++ test2.py
@@ -0,0 +1,38 @@
+import yfinance as yf
+import sys
+import pandas as pd
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+
+
+ticker = 'mo'
+data = yf.Ticker(ticker)
+
+df = data.history(period="6y")
+
+
+idx = 0
+year = 252
+stock = 1
+avr_list = []
+for close in df['Close']:
+
+    if(idx < year+1):
+        idx = idx+1
+        continue
+
+
+    pre_close = df['Close'][idx-252]
+    #print(close, pre_close, df['Dividends'][idx])
+    stock = stock + (df['Dividends'][idx]/close)
+    #print((close*stock)/pre_close - 1)
+    avr_list.append((close*stock)/pre_close - 1)
+
+    
+    idx = idx+1
+
+
+#print(sum(avr_list)/len(avr_list))
+
+print(df)
